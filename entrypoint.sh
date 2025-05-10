ecca09f61925152d038b8b4aadad7a3a0a271529
ERST <61744062+ER5TT@users.noreply.github.com>
2025-05-10 21:04:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
-if ! getent group hexo > /dev/null; then
-    addgroup -g $PGID -S hexo
+if ! getent group "$PGID" > /dev/null; then
+    addgroup -g "$PGID" -S hexo
 fi
 
 if ! getent passwd hexo > /dev/null; then
-    adduser -u $PUID -G hexo -S hexo
+    adduser -u "$PUID" -S -D -H -g "$PGID" hexo
 fi
 
 chown -R hexo:hexo /hexo
